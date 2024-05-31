d95b03cabbc337c137653145279856763bedc5bc
Hsiang-ting <79069128+Hsiang-ting@users.noreply.github.com>
2024-05-31 21:59:23 +0800

    feat: create Background class

---
+++ Background/Background.java
@@ -0,0 +1,27 @@
+package Background;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Background extends JFrame {
+    static final int DEFAULT_WIDTH = 600;
+    static final int DEFAULT_HEIGHT = 900;
+    public Background() {
+        this("", DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+    public Background(String title) {
+        this(title, DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+    public Background(int width, int height) {
+        this("", width, height);
+    }
+    public Background(String title, int width, int height) {
+        super(title);
+        setLayout(new BorderLayout());
+        setSize(width, height);
+        // display background to center of a screen 
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
