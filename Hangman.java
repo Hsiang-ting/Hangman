d95737bcc799a2940fde55fa3f22b773dd46ec0e
Hsiang-ting <79069128+Hsiang-ting@users.noreply.github.com>
2024-06-03 00:21:15 +0800

    feat: add ActionListener for Words

--- Hangman.java
+++ Hangman.java
@@ -1,13 +1,36 @@
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.event.*;
+
+import javax.swing.JLabel;
 
 import Background.*;
 import Questions.QuestionPanel;
+import WordPanel.WordPanel;
 
-public class Hangman {
+public class Hangman{
     static Background background;
     public static void main(String[] argc) {
         Background background = new Background("Hangman");
         QuestionPanel questionPnl = new QuestionPanel();
+        WordPanel wordPanel = new WordPanel();
         background.add(questionPnl, BorderLayout.NORTH);
+        background.add(wordPanel, BorderLayout.SOUTH);
+        
+        for(Component comp: wordPanel.getComponents()) {
+            comp.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    JLabel label = (JLabel)e.getComponent();
+                    String text = label.getText();
+                    System.out.println(text);
+                }
+                public void mouseEntered(MouseEvent e) {;}
+                public void mouseExited(MouseEvent e) {;}
+                public void mousePressed(MouseEvent e) {;}
+                public void mouseReleased(MouseEvent e) {;}
+            });
+            
+        }
     }
 }
